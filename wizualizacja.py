3b864f10370a8221d4e0b03789a67098f81ba1cd
Maks <maksymilian.szwabowicz@gmail.com>
2026-03-18 12:23:51 +0100

    aktualizacja wizualizacji

--- wizualizacja.py
+++ wizualizacja.py
@@ -1,10 +1,18 @@
 import matplotlib.pyplot as plt
-from main import *
-plt.scatter(y_test_final, predictions_test, color='blue', alpha=0.5, label='Dane testowe')
-max_val = float(max(y_test_final.max(), predictions_test.max()))
-plt.plot([0, max_val], [0, max_val], color="black", linewidth=2, label='Idealne dopasowanie')
-plt.xlabel('Rzeczywiste koszty')
-plt.ylabel('Przewidziane koszty')
-plt.title('Skuteczność modelu regresji')
-plt.legend()
-plt.show()
+
+def plot_regression_results(y_test, predictions):
+    plt.scatter(y_test, predictions, color='blue', alpha=0.5, label='Dane testowe')
+    max_val = float(max(y_test.max(), predictions.max()))
+    plt.plot([0, max_val], [0, max_val], color="black", linewidth=2, label='Idealne dopasowanie')
+    plt.xlabel('Rzeczywiste koszty')
+    plt.ylabel('Przewidziane koszty')
+    plt.title('Skuteczność modelu regresji')
+    plt.legend()
+    plt.show()
+
+def plot_cost_history(cost_history):
+    plt.plot(cost_history)
+    plt.xlabel('Iteracja')
+    plt.ylabel('Koszt (J)')
+    plt.title('Historia funkcji kosztu')
+    plt.show()
