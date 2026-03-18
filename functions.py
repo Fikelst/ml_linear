3fb19e8949537a2a14a02f60f47ad115b4fb2c36
Maks <maksymilian.szwabowicz@gmail.com>
2026-03-18 11:24:27 +0100

    Adding files

---
+++ functions.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def predict(X, theta):
+    return np.dot(X, theta)
+
+def compute_cost(X, y, theta):
+    m = len(y)
+    predictions = predict(X, theta)
+    cost = (1 / (2 * m)) * np.sum(np.square(predictions - y))
+    return cost
+
+
+def gradient_descent(X, y, theta, alpha, num_iters):
+    m = X.shape[0]
+    J_history = []
+    X_T = np.transpose(X)
+    for i in range(num_iters):
+        predictions = np.dot(X, theta)
+
+        error = predictions - y
+
+        gradient = (1 / m) * np.dot(X_T, error)
+
+        theta = theta - alpha * gradient
+
+        cost = (1 / (2 * m)) * np.sum(np.square(error))
+        J_history.append(cost)
+
+    return theta, J_history
